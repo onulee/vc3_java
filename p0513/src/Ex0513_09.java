b8e5adcb375145a5dda7a185d66e8d4d469f7dc1
onulee <onulee@naver.com>
2021-05-13 17:15:24 +0900

    5월 13일 수업4

--- p0513/src/Ex0513_09.java
+++ p0513/src/Ex0513_09.java
@@ -2,7 +2,9 @@
 public class Ex0513_09 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// 이름,국어,영어,수학 -> 이름,국어,영어,수학,합계,평균 출력하시오.
+		// 학생 5명, 국어,영어,수학,합계->2차원배열을 쓰시오.
+		
 
 	}
 
