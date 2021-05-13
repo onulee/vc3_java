b7f612f110137ff3aa63e838bb85b6da222514dc
onulee <onulee@naver.com>
2021-05-13 12:09:49 +0900

    5월 13일수업2

--- p0513/src/Ex0513_03.java
+++ p0513/src/Ex0513_03.java
@@ -9,23 +9,35 @@ public class Ex0513_03 {
 		// 배열섞기
 		// ball배열에 있는 것을 arr[3][3]넣기
 		
+		//0-9까지 랜덤숫자
+		int[] ball = new int[9];
+		for(int i=0;i<ball.length;i++) {
+			ball[i] = i;
+		}
+		
+		int temp=0;
+		int rno=0;
+		for(int i=0;i<50;i++) {
+			rno = (int)(Math.random()*9); //0-8
+			temp = ball[0];
+			ball[0] = ball[rno];
+			ball[rno] = temp;
+		}
 		
 		
-		
-		
-		
-//		int[][] arr = new int[3][3];
-//		for(int i=0;i<arr.length;i++) {
-//			for(int j=0;j<arr[i].length;j++) {
-//				arr[i][j]=(int)(Math.random()*10)+1;
-//			}
-//		}
-//		for(int i=0;i<arr.length;i++) {
-//			for(int j=0;j<arr[i].length;j++) {
-//				System.out.print(arr[i][j]+"\t");
-//			}
-//			System.out.println();
-//		}
+		int[][] arr = new int[3][3];
+		for(int i=0;i<arr.length;i++) {
+			for(int j=0;j<arr[i].length;j++) {
+				arr[i][j]=ball[3*i+j];  //0-9
+				// 0,1,2,3,4,5,6,7,8 
+			}
+		}
+		for(int i=0;i<arr.length;i++) {
+			for(int j=0;j<arr[i].length;j++) {
+				System.out.print(arr[i][j]+"\t");
+			}
+			System.out.println();
+		}
 		
 		// int 7개배열에 1-10까지 랜덤숫자를 넣어서 출력하시오.
 		//1. 배열 7개, 랜덤숫자 생성-넣기, 출력하기
