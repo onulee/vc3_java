8ddeb82dd558324da8e36f4a2129fbf15e4182f1
onulee <onulee@naver.com>
2021-05-17 12:05:52 +0900

    5월 17일 수업1

---
+++ p0517/src/Ex0517_03.java
@@ -0,0 +1,22 @@
+
+public class Ex0517_03 {
+
+	public static void main(String[] args) {
+		System.out.println("----------------------------------");
+		System.out.println("      [ 학생성적처리 프로그램 ]");
+		System.out.println("----------------------------------");
+		System.out.println("1.학생성적추가");
+		System.out.println("2.학생성적출력");
+		System.out.println("3.학생성적수정");
+		System.out.println("4.학생성적검색"); //중복하게 변경
+		System.out.println("5.등수처리");
+		System.out.println("6.학생성적 프로그램종료");
+		System.out.println("----------------------------------");
+		System.out.println("원하는 번호를 입력하세요.>>");
+		
+		
+		
+
+	}
+
+}
