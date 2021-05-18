56672fb878cc94e2c146fcc9f56c9c6cf0516b95
onulee <onulee@naver.com>
2021-05-18 17:44:59 +0900

    5월 18일 수업3

--- p0518/src/Ex0518_05.java
+++ p0518/src/Ex0518_05.java
@@ -8,15 +8,18 @@ public class Ex0518_05 {
 		System.out.println("두수를 입력하세요.>>");
 		int num1 = scan.nextInt();
 		int num2 = scan.nextInt();
-		int result=0; //결과값
-		if(num1>num2) {
-			result = num1;
-		}else {
-			result = num2;
-		}
+		
+		int result = add(num1,num2);
+		
+		
 		System.out.println("데이터 : "+result);
 
 	}//main메소드
 	
-
+	//두수를 더해서 리턴해주는 메소드
+	static int add(int num1,int num2) {
+		int result = num1+num2;
+		return result;
+	}
+	
 }
