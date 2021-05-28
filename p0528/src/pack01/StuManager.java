2ea46e41bf8d75a099f972f745712298ddd582b4
onulee <onulee@naver.com>
2021-05-28 16:06:21 +0900

    5월 28일 수업3

--- p0528/src/pack01/StuManager.java
+++ p0528/src/pack01/StuManager.java
@@ -7,12 +7,15 @@ public class StuManager {
 	static Scanner scan = new Scanner(System.in);
 	public static void main(String[] args) {
 		//학생성적저장 객체배열
-		ArrayList list = new ArrayList();
+		ArrayList list = new ArrayList();  
 		Stu_process p = new Stu_process();
 		
 		while(true) {
 			System.out.println("[ 학생성적 프로그램 ]");
 			System.out.println("1. 학생성적추가");
+			System.out.println("2. 학생성적출력");
+			System.out.println("3. 학생성적수정");
+			System.out.println("4. 학생성적검색");
 			System.out.println("-----------------------------------");
 			System.out.println("원하는 번호를 입력하세요.>>");
 			int choice = scan.nextInt();
@@ -21,11 +24,22 @@ public class StuManager {
 			case 1: //학생성적추가
 				//1.이름,국어,영어,수학
 				p.student_insert(list);
-				
-				
-				
-				
-			 break;
+			    break;
+			case 2: //학생성적출력
+				//1.상단출력 2.for을 사용해서 3.list에 있는 데이터 출력
+				p.student_print(list);
+				break;
+			case 3: //학생성적수정
+				//1.수정할 학생검색-비교 2.과목선택 3.점수입력 4.점수수정
+				p.student_update(list);
+				break;
+			case 4: //학생성적검색
+				//1.검색할 이름입력 2.for반복 3.if문비교 4.검색 출력
+				p.student_search(list);
+				break;
+
+			
+			
 			
 			
 			}//switch
--- p0528/src/pack01/Stu_process.java
+++ p0528/src/pack01/Stu_process.java
@@ -25,14 +25,102 @@ public class Stu_process {
 			System.out.println("수학점수를 입력하세요.>>");
 			math = scan.nextInt();
 			//list 저장
-			list.add(new Student(stu_name,kor,eng,math));
+			Student s = new Student(stu_name,kor,eng,math);
+			list.add(s);
 		}//while
+	}//student_insert
+	
+	//학생성적출력 메소드
+	void student_print(ArrayList list) {
+		System.out.print("번호\t이름\t국어\t영어\t수학\t합계\t평균\t등수\n");
+		System.out.println("----------------------------------------------------------");
 		
+		for(int i=0;i<list.size();i++) {
+			Student s = (Student) list.get(i); // 형변환 Object->Student
+			System.out.print(s.stu_num+"\t");
+			System.out.print(s.stu_name+"\t");
+			System.out.print(s.kor+"\t");
+			System.out.print(s.eng+"\t");
+			System.out.print(s.math+"\t");
+			System.out.print(s.total+"\t");
+			System.out.printf("%.2f\t",s.avg);
+			System.out.print(s.rank+"\n");
+		}
+	}//student_print
+	
+	//학생성적수정 메소드
+	void student_update(ArrayList list) {
+		System.out.println("수정할 학생이름을 입력하세요.>>");
+		String stu_name = scan.next();
 		
+		// list
+		int check_flag=0;
+		for(int i=0;i<list.size();i++) {
+			Student s = (Student)list.get(i); //list에서 1개씩 가져옴
+			if(stu_name.equals(s.stu_name)) {
+				System.out.println("수정하고자 하는 학생을 찾았습니다.");
+				System.out.println("[ 수정과목선택 ]");
+				System.out.println("1.국어 2.영어 3.수학");
+				System.out.println("원하는 번호를 선택하세요.>>");
+				int choice = scan.nextInt();
+				switch (choice) {
+				case 1: //국어
+					System.out.println("현재점수 : "+s.kor);
+					System.out.println("수정하고 싶은 점수를 입력하세요.>>");
+					s.kor = scan.nextInt();
+					break;
+				case 2:
+					System.out.println("현재점수 : "+s.eng);
+					System.out.println("수정하고 싶은 점수를 입력하세요.>>");
+					s.eng = scan.nextInt();
+					break;
+				case 3:
+					System.out.println("현재점수 : "+s.math);
+					System.out.println("수정하고 싶은 점수를 입력하세요.>>");
+					s.math = scan.nextInt();
+					break;
+				}//switch
+				
+				s.total = s.kor+s.eng+s.math; //합계수정
+				s.avg = s.total/3.0;  //평균수정
+				check_flag=1;
+			}//if
+		}//for
 		
+		if(check_flag==0) {
+			System.out.println("수정할 학생을 찾지 못했습니다.");
+		}
+	}//student_update
+	
+	//학생성적검색 메소드
+	void student_search(ArrayList list) {
+		System.out.println("검색할 학생이름을 입력하세요.>>");
+		String stu_name = scan.next(); //홍
 		
+		System.out.print("번호\t이름\t국어\t영어\t수학\t합계\t평균\t등수\n");
+		System.out.println("----------------------------------------------------------");
 		
-	}
+		int check_flag=0; //없다.
+		for(int i=0;i<list.size();i++) {
+			Student s = (Student)list.get(i);
+			if(s.stu_name.contains(stu_name)) { //홍길동,홍길자,홍길순,박수홍
+				System.out.print(s.stu_num+"\t");
+				System.out.print(s.stu_name+"\t");
+				System.out.print(s.kor+"\t");
+				System.out.print(s.eng+"\t");
+				System.out.print(s.math+"\t");
+				System.out.print(s.total+"\t");
+				System.out.printf("%.2f\t",s.avg);
+				System.out.print(s.rank+"\n");
+				check_flag=1; //검색할 대상을 찾음.
+			}
+		}//for
+		
+		if(check_flag==0) {
+			System.out.println("찾는 데이터가 없습니다. 다시 검색하세요.");
+		}
+	}//student_search
+	
 	
 	
 }
